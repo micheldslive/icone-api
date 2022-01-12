545d76af2e435946e9306fca190c84fcf91641a7
Michel Domingos <micheldslive@gmail.com>
2022-01-12 11:50:37 -0300

    change api description

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const port: any = process.env.PORT || 4000;
 
-  const APP_NAME = process.env.npm_package_name || "myapp";
+  const APP_NAME = process.env.npm_package_name || "Api-Icone";
   const APP_VERSION = process.env.npm_package_version || "1.0";
 
   const options = new DocumentBuilder()
     .setTitle(APP_NAME)
-    .setDescription(`The ${APP_NAME} API description`)
+    .setDescription(`Bem vindo(a) a ${APP_NAME}, API do teste prático do Gama Xp 39. Para consultar mais informações, acesse nossa documentação abaixo.`)
     .setVersion(APP_VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, options);
